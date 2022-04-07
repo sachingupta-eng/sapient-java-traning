72948b05669938b381f438decbe728ac77af2629
Sachin Gupta <sacgupta4@publicisgroupe.net>
2022-04-07 10:38:50 +0530

    Day-1

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+
+public class Book {
+private String  title;
+private float price;
+private STATUS status;
+
+
+}
---
+++ src/Natural.java
@@ -0,0 +1,10 @@
+
+public class Natural {
+	public static void main(String args[]) {
+		int i = 1;
+		while (i <= 20) {
+			System.out.print(i + "\t");
+			i++;
+		}
+	}
+}
